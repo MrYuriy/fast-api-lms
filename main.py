a88fb9a8956eed8a5ed047537d30983db65723ec
MrYuriy <yuriybabiyk@gmail.com>
2024-06-03 22:00:41 +0200

    first commit with inicial API setup

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
+from typing import Optional, List
+
+app = FastAPI(
+    title="Fast API LMS",
+    description="LMS for managing students and courses.",
+    version="0.0.1",
+    contact={
+        "name": "Gwen",
+        "email": "yuriibabiuk@gmail.com",
+    },
+    license_info={
+        "name": "MIT",
+    },
+)
+
+users = []
+
+class User(BaseModel):
+    email: str
+    is_active: bool
+    bio: Optional[str] = None
+
+
+@app.get("/users", response_model=List[User])
+async def get_user():
+    return users
+
+
+@app.post("/users")
+async def create_user(user: User):
+    users.append(user)
+    return users
+
+@app.get("/user/{id}")
+async def get_user(
+    id: int = Path(..., description="The ID of user you want to retrive", gt=2),
+    q: str = Query(None, max_length=5)
+):
+    return { "user": users[id], "query": q }
